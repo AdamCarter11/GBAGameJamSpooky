ed183f863ebe1bf5786268e24ce373ab88068b80
Adam <57573881+AdamCarter11@users.noreply.github.com>
2024-09-15 14:53:03 -0700

    Input done

--- GBAGameJam/Assets/Scripts/Player/PlayerMovement.cs
+++ GBAGameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float jumpPower = 10f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    Rigidbody2D rb;
+    float horizontalMove;
+    bool isFacingRight = true;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+        if(!isFacingRight && horizontalMove > 0)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontalMove < 0)
+        {
+            Flip();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Jump(InputAction.CallbackContext context)
     {
-        
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, .2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMove = context.ReadValue<Vector2>().x;
     }
 }
